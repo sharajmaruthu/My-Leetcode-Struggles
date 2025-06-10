c75ba2c444a310ca688591e23fc557d8b1c9a1b7
Sharaj M M <simplysharaj@gmail.com>
2025-06-10 19:14:20 +0530

    Sum 5

---
+++ medium/5. Longest Palindromic Substring.py
@@ -0,0 +1,29 @@
+#SOLUTION
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s or len(s) < 1:
+            return ""
+        
+        start, end = 0, 0
+        
+        def expandAroundCenter(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+        
+        for i in range(len(s)):
+            l1, r1 = expandAroundCenter(i, i)
+            l2, r2 = expandAroundCenter(i, i + 1)
+            
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+                
+        return s[start:end+1]
