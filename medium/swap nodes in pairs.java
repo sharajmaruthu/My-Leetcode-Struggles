335a664e4db84e524012bfe0f0787ba390d9ecf6
Sharaj M M <simplysharaj@gmail.com>
2025-07-21 20:45:43 +0530

    Create swap nodes in pairs.java

---
+++ medium/swap nodes in pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode ptr = head;
+        while(ptr!=null && ptr.next!=null){
+            int temp = ptr.val;
+            ptr.val = ptr.next.val;
+            ptr.next.val = temp;
+            ptr = ptr.next.next;
+        }
+
+        return head;
+    }
+}
